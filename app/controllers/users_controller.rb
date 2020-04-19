ff1b37393194696d9f57ceea2f44b463b3cdd118
Ross <Rharper9@gmail.com>
2020-04-18 21:58:04 -0500

    Added user creation and editing pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,36 @@
 class UsersController < ApplicationController
 
     def new
-
+        @user = User.new
     end
 
     def create
-
-    end
+        @user = User.new(user_params)
     
+        if @user.save
+          redirect_to @user
+        else
+          redirect_to render :new
+        end
+    end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+
+        if @user.update(user_params)
+            redirect_to edit_user_path
+          else
+            render :edit
+          end
+    end
+
+    private
+        
+        def user_params
+            params.require(:user).permit(:username, :email, :password)
+        end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
     validates :username, presence: true
-    validates :username, format: { with: /\A[a-zA-Z]+\z/,
-    message: "only allows letters" }
-    validates :username, length: {minimum: 2}
+    #validates :username, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
+    #validates :username, length: {minimum: 2}
     validates :email, presence: true
     validates :password, presence: true
 end
